c1e61664589c0deaf678e0714a563a8b95cf79af
shahmdsiddik1 <147234668+shahmdsiddik1@users.noreply.github.com>
2025-12-18 22:43:02 +0530

    Update Campus.jsx

--- Campus.jsx
+++ Campus.jsx
@@ -6,16 +6,21 @@ import gallery_2 from '../../../assets/gallery-2.png'
 import gallery_3 from '../../../assets/gallery-3.png'
 import gallery_4 from '../../../assets/gallery-4.png'
 import white_arrow from '../../../assets/white-arrow.png'
+
 const Campus = () => {
   return (
-    <div className='campus'>
-        <div className="gallery">
-            <img src={gallery_1} alt="" />
-             <img src={gallery_2} alt="" />
-              <img src={gallery_3} alt="" />
-               <img src={gallery_4} alt="" />
-        </div>
-      <button className='btn dark-btn'>See more here <img src={white_arrow} alt="" /></button>
+    <div className="campus">
+      <div className="gallery">
+        <img src={gallery_1} alt="Campus view 1" />
+        <img src={gallery_2} alt="Campus view 2" />
+        <img src={gallery_3} alt="Campus view 3" />
+        <img src={gallery_4} alt="Campus view 4" />
+      </div>
+
+      <button className="btn dark-btn">
+        See more here
+        <img src={white_arrow} alt="arrow icon" />
+      </button>
     </div>
   )
 }
